f87ce958f069dab53304dd47ff654e28cbe379b2
Den-is-Ka <D_K81@mail.ru>
2025-07-02 15:08:00 +0500

    немного поворил

--- src/povtor.py
+++ src/povtor.py
@@ -1,2 +1,36 @@
-def nen():
-    pass
+
+age = "poe[albcm"
+roui = '1234'
+print(age + ' † ' + roui)
+
+if __name__=='__main__':
+    print()
+
+воды_в_день = 7
+день = 7
+result = воды_в_день * день
+print(result)
+
+if __name__=='__main__':
+    result
+
+price = 60000
+mounts = 36
+result = price // mounts
+print(result)
+
+if __name__=='__main__':
+    result
+
+сумма_с_которой_неуплачен_налог = int('30000')
+налог = 0.1
+
+result = сумма_с_которой_неуплачен_налог * налог
+
+print(f'Сумма неуплаченных налогов с {сумма_с_которой_неуплачен_налог} составит {int(result)}')
+
+if __name__=='__main__':
+    print
+
+
+
