8890aa7b997e667a46d3158aa4336929c3a103f1
Den-is-Ka <D_K81@mail.ru>
2025-07-03 10:28:31 +0500

    начал следующий урок

---
+++ src/tipi_dannix.py
@@ -0,0 +1,80 @@
+from tokenize import endpats
+
+age = "poe[albcm"
+roui = '1234'
+print(age + ' † ' + roui)
+
+if __name__=='__main__':
+    print()
+
+воды_в_день = 7
+день = 7
+result = воды_в_день * день
+print(result)
+
+if __name__=='__main__':
+    result
+
+price = 60000
+mounts = 36
+result = price // mounts
+print(result)
+
+if __name__=='__main__':
+    result
+
+сумма_с_которой_неуплачен_налог = int('30000')
+налог = 0.1
+
+result = сумма_с_которой_неуплачен_налог * налог
+
+print(f'Сумма неуплаченных налогов с {сумма_с_которой_неуплачен_налог} составит {int(result)}')
+
+if __name__=='__main__':
+    print
+
+цена_продуктов = "1860"
+цена_продуктов_числовая = int(цена_продуктов)
+кол_во_гостей = 6
+чек_гостя = round(цена_продуктов_числовая / кол_во_гостей)
+print(f"Привет! покушал хорошо, верни {чек_гостя} деньга")
+
+if __name__=='__main__':
+    print
+
+print('India', 'Juliett',' Kilo', sep=', = ')
+
+if __name__=='__main__':
+    print
+
+print('India\nJuliett\nKilo')
+
+if __name__=='__main__':
+    print
+
+print('***\n***\n***')
+
+if __name__=='__main__':
+    print
+
+print('*\n***\n*****\n***\n*')
+
+if __name__=='__main__':
+    print
+
+print('''*
+***
+*****
+***
+*''')
+
+if __name__=='__main__':
+    print
+
+print('India', end='/')
+print('Juliett', end='/')
+print('Kilo', end='/')
+
+if __name__=='__main__':
+    print
+
