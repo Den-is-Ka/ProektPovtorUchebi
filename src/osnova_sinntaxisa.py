52b542860e106cf84b7bf80717026d6e50ce5b32
Den-is-Ka <D_K81@mail.ru>
2025-07-30 15:05:09 +0500

    продолжаю урок 3.1 совместить с домашним пк

--- src/osnova_sinntaxisa.py
+++ src/osnova_sinntaxisa.py
@@ -1,54 +1,153 @@
-mesadge_num = int(input("введите число: "))
-if mesadge_num > 0:
-    print('Есть сообщения')
-else:
-    print('Сообщений нет')
+# mesadge_num = int(input("введите число: "))
+# if mesadge_num > 0:
+#     print('Есть сообщения')
+# else:
+#     print('Сообщений нет')
+#
+# if __name__ == '__main__':
+#     print
+#
+#
+# слово_1 = input("введите слово: ")
+# слово_2 = input("введите слово: ")
+# слово_3 = input("введите слово: ")
+#
+# counter = 0
+# if 'ч' in слово_1:
+#     counter += 1
+# if 'ч' in слово_2:
+#     counter += 1
+# if 'ч' in слово_3:
+#     counter += 1
+#
+# print(f'Набралось {counter} слов')
+#
+# if __name__ == '__main__':
+#     print
+#
+# residency = int(input("введите время жизни на одном месте: "))
+# salary = int(input("введите вашу зарплату: "))
+# experience = int(input("введите стаж работы: "))
+#
+# counter = 0
+# if residency >= 2:
+#     counter += 1
+# else:
+#     print('Мало жили на одном месте')
+# if salary >= 50000:
+#     counter += 1
+# else:
+#     print('Мало зарабатываете')
+# if experience >= 2:
+#     counter += 1
+# else:
+#     print('Малый стаж')
+#
+# if counter == 3:
+#     print('Вам одобрен кредит')
+# elif counter == 2:
+#     print('Вам немного не хватает')
+# else:
+#     print('Вам не хватает, идите работать')
+#
+# print(f'ваш счетчик составляет {counter} балов')
+#
+# if __name__ == '__main__':
+#     print
+from src.tipi_dannix import result
 
-if __name__ == '__main__':
-    print
+# вес_сумки = int(input('введите вес: '))
+#
+# if вес_сумки <= 5:
+#     print('Можно взять в салон')
+# else:
+#     print('Нужно сдать в багаж')
+#
+# if __name__ == '__main__':
+#     print
 
+# our_speed = int(input())
+# max_speed = int(input())
+#
+# if max_speed < our_speed:
+#     print(False)
+# else:
+#     print(True)
 
-слово_1 = input("введите слово: ")
-слово_2 = input("введите слово: ")
-слово_3 = input("введите слово: ")
+# число = int(input('Введите число: '))
+#
+# проверка = число % 10
+#
+# if проверка == 1:
+#     print(True)
+# else:
+#     print(False)
+#
+# денег_на_счете = int(input('Введите деньгу: '))
+# курс = int(input('Введите курс: '))
+#
+# if денег_на_счете >= курс:
+#     print(True)
+# else:
+#     print(False)
 
-counter = 0
-if 'ч' in слово_1:
-    counter += 1
-if 'ч' in слово_2:
-    counter += 1
-if 'ч' in слово_3:
-    counter += 1
+# число = int(input('введите число: '))
+# if число == 0:
+#     print('ноль')
+# elif число == 1:
+#     print('одын')
+# elif число == 2:
+#     print('два')
+# elif число == 3:
+#     print('три')
+# elif число == 4:
+#     print('четыре')
+#
+# число1 = int(input('число'))
+# число2 = int(input('число'))
+# число3 = int(input('число'))
+#
+# result1 = число1 + число2 + число3
+#
+# print(result1)
+#
+# if __name__ == '__main__':
+#     print
 
-print(f'Набралось {counter} слов')
+# число1 = int(input('число'))
+#
+# число2 = число1 * число1
+# число3 = число1 * число1 * число1
+#
+# print(число2)
+# print(число3)
 
-if __name__ == '__main__':
-    print
+# число1 = int(input())
+# число2 = int(input())
+# число3 = int(input())
+#
+# минимум = min(число1, число2, число3)
+# максимум = max(число1, число2, число3)
+# среднее = (число1 + число2 + число3) / 3
+#
+# print(f'{минимум} число')
+# print(f'{максимум} число')
+# print(f'{среднее} число')
 
-residency = int(input("введите время жизни на одном месте: "))
-salary = int(input("введите вашу зарплату: "))
-experience = int(input("введите стаж работы: "))
+# баллы = int(input())
+# возраст = int(input())
+#
+# if баллы >= 20 and возраст >=18:
+#     print('можете катать')
+# else:
+#     print('хер а не водить')
 
-counter = 0
-if residency >= 2:
-    counter += 1
-else:
-    print('Мало жили на одном месте')
-if salary >= 50000:
-    counter += 1
-else:
-    print('Мало зарабатываете')
-if experience >= 2:
-    counter += 1
-else:
-    print('Малый стаж')
-
-if counter == 3:
-    print('Вам одобрен кредит')
-elif counter == 2:
-    print('Вам немного не хватает')
-else:
-    print('Вам не хватает, идите работать')
-
-print(f'ваш счетчик составляет {counter} балов')
+# x = int(input('число'))
+# y = int(input('число'))
+# z = int(input('число'))
+#
+# if x > 100 or z > 100 or y > 100:
+#     print('NO с собой')
+# else:
+#     print('beri')
 
---
+++ src/spiski_3_1.py
@@ -0,0 +1,90 @@
+# цифры = [2, 4, 6, 8 ,10]
+#
+# for циф in цифры:
+#     print(циф)
+#
+# цифры = [2, 4, 6, 8 ,10]
+#
+# элемент = цифры[4]
+# print(элемент)
+#
+# items = ["палатка", "спички", "одеяло", "нож", "зефирки", "крючок", "консервы"]
+#
+# index_1 = int(input())
+# index_2 = int(input())
+#
+# print(items[index_1])
+# print(items[index_2])
+# print(type(items[index_2]))
+
+# items = ["палатка", "спички", "одеяло", "нож", "зефирки", "крючок", "консервы"]
+#
+# items.append('24')
+# print(items)
+# print(len(items))
+# lengs = len(items)
+# print(lengs)
+# for item in items:
+#   print(item)
+
+
+# fruits = ["яблоко", "груша"]
+# fruits_2 = ["лимон", "банан", "киви"]
+# # fruits.extend(["лимон", "банан", "киви"])
+# fruits_3 = fruits + fruits_2
+#
+#
+# for fruit in fruits_3:
+#   print(fruit)
+
+# list_1 = ["apple", "banana", "blueberry", "strawberry", "melon"]
+# list_2 = ["blue", "orange", "blue", "blue", "yellow"]
+# list_3 = [True, False, True, True, False, True]
+#
+# list_1_len = len(list_1)
+# list_2_len = len(list_2)
+# list_3_len = len(list_3)
+#
+# print(list_1_len, list_2_len, list_3_len)
+
+
+# list_1 = ["apple", "banana", "strawberry", "melon"]
+# print(len(list_1))
+# print(type(len(list_1)))
+# print(type(list_1))
+
+# fruits = ["яблоко", "груша"]
+# fruits_2 = ["лимон", "банан", "киви"]
+#
+# fruits.extend(fruits_2)
+# print(fruits)
+# print(type(fruits))
+
+# list_1 = ["apple", "banana", "strawberry", "melon"]
+# list_1[2] = 'ersfsdf'
+# pos = 1
+# print(list_1)
+# print(list_1[pos])
+# pos += 1
+# print(list_1[pos])
+
+
+# list_1 = ["apple", "banana", "blueberry", "strawberry", "melon"]
+# print(list_1[2:])
+
+list = ["apple", "арбуз", "banana", "корень", "blueberry", "strawberry", "железо", "melon"]
+list_en = list[0:1] + list[2:3] + list[4:6] + list[-1:]
+list_ru = list[1:2] + list[3:4] + list[-2:-1]
+
+print(list_en)
+print(list_ru)
+print(list)
+
+print(type(list_en))
+print(type(list_ru))
+
+
+
+
+
+
