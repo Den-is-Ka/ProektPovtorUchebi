2033e11d60c1a0440e289b6d722824a5388bacd1
user <d_k81@mail.ru>
2025-08-03 21:15:14 +0500

    продвин функции 7.1

---
+++ src/prodvin_func_7_1.py
@@ -0,0 +1,52 @@
+#
+# def greet(name, greeting='Hoi', punctuation='.'):
+#     print(f"{greeting}, {name}{punctuation}")
+#
+#
+#
+#
+# if __name__ == '__main__':
+#     greet('Nora')
+#     greet('NoZa', greeting='Пириветь')
+#     greet('NorKa', greeting='Пириветь', punctuation='!!@!')
+#     greet('Nora')
+
+
+# def shopping_list(*items, **comments):
+#     for item in items:
+#         if item in comments:
+#             print(f'{item} ({comments[item]})')
+#         else:
+#             print(f'{item}')
+#
+#
+# if __name__ == '__main__':
+#     shopping_list("хлеб", "молоко", 'крупа', "сыр", хлеб="чёрный", молоко="1 литр")
+
+# def sum_numbers(*numbers):
+#     print("Числа:", numbers)
+#     print("Сумма:", sum(numbers))
+#
+#
+# if __name__ == '__main__':
+#     sum_numbers(2, 4, 6)
+#     sum_numbers(10)
+#     sum_numbers()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
